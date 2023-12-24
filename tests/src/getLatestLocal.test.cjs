a00b8c7506ffb782d7990c9054874e79a04a7dbe
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2023-12-23 22:27:37 -0500

    Stub out unit test file

---
+++ tests/src/getLatestLocal.test.cjs
@@ -0,0 +1,29 @@
+'use strict';
+
+/**
+ * @file Unit tests for checking the most recent local version of the app.
+ */
+
+const getLatestLocal = require('../../src/getLatestLocal.cjs');
+
+describe('getLatestLocal.cjs', () => {
+  let options;
+
+  beforeEach(() => {
+    options = {
+      verbose: true,
+      customLogger: vi.fn()
+    };
+  });
+
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  test('Stub for coverage', async () => {
+    const result = await getLatestLocal(options);
+
+    expect(result)
+      .toEqual('0.0.0');
+  });
+});
