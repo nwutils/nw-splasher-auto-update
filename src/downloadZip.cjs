65f072b75a7d8cc1c6a2afd42af65bbbf6cf39af
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2023-12-30 19:05:09 -0500

    change request-progress to easydl

--- src/downloadZip.cjs
+++ src/downloadZip.cjs
@@ -4,21 +4,50 @@
  * @file Checks the most recent local version of the app
  */
 
-const fs = require('fs');
+const EasyDl = require('easydl');
 
-const { OPTIONS } = require('../api-type-definitions.cjs');
+const {
+  DOWNLOADPROGRESS,
+  OPTIONS
+} = require('../api-type-definitions.cjs');
 
-const { ZIPS_LOCATION } = require('./constants.cjs');
+const helpers = require('./helpers.cjs');
 
 /**
  * Downloads the zip file to the download location.
  *
- * @param {OPTIONS} options       The user's options object
- * @param {string}  downloadPath  The zip file URL provided by the user
+ * @param  {OPTIONS} options       The user's options object
+ * @param  {string}  downloadUrl   The zip file URL provided by the user
+ * @param  {string}  latestRemote  The latest version number of the app we are downloading
+ * @return {Promise}               Returns a promise to await download completion
  */
-async function downloadZip (options, downloadPath) {
-  console.log('The downloadZip function is a stub and needs implemented');
-  console.log(downloadPath);
+async function downloadZip (options, downloadUrl, latestRemote) {
+  const zipFilePath = helpers.getZipFilePath(latestRemote);
+  const downloadOptions = {
+    existBehavior: 'overwrite',
+    maxRetry: options.autoUpdate.downloadRetries,
+    reportInterval: 1250
+  };
+
+  return new EasyDl(downloadUrl, zipFilePath, downloadOptions)
+    .on(
+      'progress',
+      /**
+       * Callback for when progress events occur while downloading zip file.
+       *
+       * @param {DOWNLOADPROGRESS} downloadProgress  Details about the progress of the download
+       */
+      function (downloadProgress) {
+        options.autoUpdate.onUpdate({ downloadProgress });
+      }
+    )
+    .wait()
+    .then((completed) => {
+      console.log('Downloaded?', completed);
+    })
+    .catch((error) => {
+      console.log('[error]', error);
+    });
 };
 
 module.exports = downloadZip;
